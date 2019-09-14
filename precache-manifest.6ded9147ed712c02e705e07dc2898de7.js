69e0e00295b642abd336363b2060cb25f3eaa208
charlesnzubechukwu000@gmail.com <charlesnzubechukwu000@gmail.com>
2019-09-14 19:36:00 +0100

    Updates

---
+++ precache-manifest.6ded9147ed712c02e705e07dc2898de7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcb92f24891aebdf1aa0c0a38fa7ddba",
+    "url": "/fyrozine/index.html"
+  },
+  {
+    "revision": "4190bb41a954e3b3d512",
+    "url": "/fyrozine/static/css/main.8403c495.chunk.css"
+  },
+  {
+    "revision": "2eeff4c0b69b98b7cc96",
+    "url": "/fyrozine/static/js/2.6901e241.chunk.js"
+  },
+  {
+    "revision": "4190bb41a954e3b3d512",
+    "url": "/fyrozine/static/js/main.f2391df9.chunk.js"
+  },
+  {
+    "revision": "f9c20400ab4105fe6c93",
+    "url": "/fyrozine/static/js/runtime~main.9acf398c.js"
+  },
+  {
+    "revision": "3dcf0ae6a78a6a64ae1e3f2406cefa3b",
+    "url": "/fyrozine/static/media/JuliusSansOne-Regular.3dcf0ae6.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/fyrozine/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "2edf1f56d51e4cab997f000fb89ff549",
+    "url": "/fyrozine/static/media/fyrozine_login_mobile.2edf1f56.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/fyrozine/precache-manifest.6ded9147ed712c02e705e07dc2898de7.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fyrozine/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
